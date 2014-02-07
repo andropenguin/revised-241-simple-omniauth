f383944f5d88f947fb26bb5a57761dd8c5ddf365
andropenguin <andropenguin@gmail.com>
2014-02-07 18:19:39 +0900

    make twitter_ominauth_helper.rb and modify some files

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,17 @@
 class SessionsController < ApplicationController
+  include TwitterOmniauthHelper
+
   def create
     auth = request.env["omniauth.auth"]
-    user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
-    session[:user_id] = user.id
+    # user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
+    # session[:user_id] = user.id
+    set_auth_session(auth)
     redirect_to root_url, :notice => "Signed in!"
   end
 
   def destroy
-    session[:user_id] = nil
+    # session[:user_id] = nil
+    set_auth_session(nil)
     redirect_to root_url, :notice => "Signed out!"
   end
 end
---
+++ app/helpers/twitter_omniauth_helper.rb
@@ -0,0 +1,38 @@
+module TwitterOmniauthHelper
+  def signed_in?
+    session[:oauth_token] != nil
+  end
+
+  def create_client
+# This is not bad use. It does not work.
+# client = Twitter::Client.new(
+# :consumer_key => session[:consumer_key],
+# :consumer_secret => session[:consumer_secret],
+# :oauth_token => session[:oauth_token],
+# :oauth_token_secret => session[:oauth_token_secret]
+# )
+    client = Twitter::REST::Client.new do |config|
+      config.consumer_key = session[:consumer_key]
+      config.consumer_secret = session[:consumer_secret]
+      config.access_token = session[:oauth_token]
+      config.access_token_secret = session[:oauth_token_secret]
+    end
+    return client
+  end
+
+  def set_auth_session(auth)
+    if auth
+      session[:consumer_key] = auth.extra.access_token.consumer.key
+      session[:consumer_secret] = auth.extra.access_token.consumer.secret
+      session[:oauth_token] = auth.credentials.token
+      session[:oauth_token_secret] = auth.credentials.secret
+      session[:username] = auth.extra.access_token.params[:screen_name]
+    else
+      session[:consumer_key] = nil
+      session[:consumer_secret] = nil
+      session[:oauth_token] = nil
+      session[:oauth_token_secret] = nil
+      session[:username] = nil
+    end
+  end
+end
